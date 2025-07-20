60b6573890204b24b64d2093fe446a9c41695a40
yagodfreitas <sucessonegociodigital@gmail.com>
2025-07-19 22:21:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const PORT = process.env.PORT || 3000;
 // Serve os arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, "public")));
 
+// Rota principal para servir o index.html
 app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
