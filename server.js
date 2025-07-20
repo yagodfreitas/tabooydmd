8e5c1fc88d355c74797222542404695ca3a61238
yagodfreitas <sucessonegociodigital@gmail.com>
2025-07-19 21:48:11 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,24 +3,20 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// Porta dinâmica para o Render
 const PORT = process.env.PORT || 3000;
 
-// Servir arquivos estáticos da pasta atual
 app.use(express.static(__dirname));
 
-// Rota principal
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Lógica de socket
 io.on('connection', (socket) => {
   console.log('Um jogador conectou: ' + socket.id);
 
   socket.on('mensagem', (data) => {
     console.log('Mensagem recebida:', data);
-    io.emit('mensagem', data); // broadcast
+    io.emit('mensagem', data);
   });
 
   socket.on('disconnect', () => {
@@ -28,7 +24,6 @@ io.on('connection', (socket) => {
   });
 });
 
-// Inicialização do servidor
 http.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
