32c0de0bac3f1bbb2a13e1ae8c64e9f68daa9381
yagodfreitas <sucessonegociodigital@gmail.com>
2025-07-19 22:33:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve os arquivos estáticos da pasta public
+// Serve arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, "public")));
-
-// Rota principal
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
